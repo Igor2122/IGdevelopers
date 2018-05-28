b83dc558c50b30feb23569527d37b7d3e29a4eef
Igor <igor_pavlenko@yahoo.com>
2018-05-28 18:35:10 +0300

    First commit

--- js/app.js
+++ js/app.js
@@ -3,10 +3,39 @@ $(document).ready(function() {
 //   $('h1').hide();
    
    
-   
+$('.carousel-control.left').click(function() {
+  $('#myCarousel').carousel('prev');
+});
+
+$('.carousel-control.right').click(function() {
+  $('#myCarousel').carousel('next');
+}); 
    
    
    
    
    
 });
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+  
+const modalImg = document.querySelector('website-image');
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(test);
+
+
+}); // document ready function
